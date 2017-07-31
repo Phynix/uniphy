65570034d2f3b1f0d602468a9b86a5e876caa603
Johannes Sebastian Lade <johannes.lade@yahoo.com>
2017-07-31 23:01:53 +0200

    Rewrote todo for autorecognition.

--- test_decorators.py
+++ test_decorators.py
@@ -104,7 +104,7 @@ class TestTypeChecked(unittest.TestCase):
                 pass
 
     def test_works_with_args_kwargs(self):
-        '''TODO: Define behaviour when *args and **kwargs are beeing used.'''
+        # TODO: Define behaviour when *args and **kwargs are beeing used.
         @decorators.type_checked
         def test_func(*args: int, **kwargs: int):
             pass
